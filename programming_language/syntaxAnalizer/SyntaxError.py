daedf8c2271ee44fd10e7c87a412553049a3f93c
AlejandroCD2k3 <alejandrocd2k3@gmail.com>
2025-02-12 15:23:25 -0500

    refactor: Changing program's structure

--- programming_language/syntaxAnalizer/SyntaxError.py
+++
@@ -1,4 +0,0 @@
-class SyntaxError(Exception):
-    def __init__(self, message, position):
-        super().__init__(f"Syntax error at position {position}: {message}")
-        self.position = position
